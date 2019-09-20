9c3f117043444f057034fc3d497226e079a0f9ce
redhanoormansyah <redhanoormansyah30@gmail.com>
2019-09-20 14:38:22 +0700

    Lion Parcel Versi Beta

---
+++ assets/bootstrap/js/function.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function () {
+	$('.testiSlide').slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 1500,
+		responsive: [{
+			breakpoint: 850,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+			}
+		}]
+	});
+});
